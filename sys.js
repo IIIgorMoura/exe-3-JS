d520efefae033e5c0a4b2ebf776fcfa76d84838b
IIIgorMoura <igor2007moura@gmail.com>
2023-08-08 10:06:42 -0300

    levemente melhorado

--- sys.js
+++
@@ -1,8 +0,0 @@
-// button.addEventListener('click', function() {
-// let ref = document.querySelector(':root');
-// let refStyle = getComputedStyle(ref);
-// let corFundo = refStyle.getPropertyValue('--corPage');
-// let acesso = document.getElementById('txt');
-
-// let valor = document.getElementById('senha').innerHTML;
-
