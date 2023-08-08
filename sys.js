4a6052de7cea1de9455853394fabd7b105728274
IIIgorMoura <igor2007moura@gmail.com>
2023-08-08 09:56:16 -0300

    teoricamente foi

--- sys.js
+++ sys.js
@@ -6,27 +6,3 @@
 
 // let valor = document.getElementById('senha').innerHTML;
 
-// console.log(valor);
-
-// });
-
-let valor = document.querySelector("#senha");
-let botao = document.querySelector("#entrar");
-
-botao.addEventListener('click', function() {
-    let valInput = valor.value;
-
-    console.log(valInput);
-});
-
-if (valor>=1000 && valor<2000) {
-    console.log(valor)
-}
-
-else if (valor>=2000 && valor<3000) {
-    console.log("jose")
-}
-
-else if (valor>=3000) {
-    console.log()
-}
